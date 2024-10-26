5c10bb76c529857c1ae55378e70576e023acb832
jony2511 <141722597+jony2511@users.noreply.github.com>
2024-10-26 13:09:21 +0600

    Added Jacobi and Gauss-Seidel method

--- Equation Solver.cpp
+++ Equation Solver.cpp
@@ -229,6 +229,79 @@ void rungekutta(){
     
 }
 
+void jacobiMethod(vector<int> vec[], int n)
+{
+    vector<double> x(n,0.0),x1(n, 0.0);
+    int maxIt=1000;
+    double tol=0.00001;
+    cout<<endl<<"**********JACOBI ITERATIVE METHOD********"<<endl;
+        for (int it = 0; it < maxIt; it++)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            double sum = vec[i][n];
+            for (int j = 0; j < n; j++)
+            {
+                if (i != j)
+                    sum -= vec[i][j] * x[j];
+            }
+            x1[i] = sum / vec[i][i];
+        }
+
+        double err = 0.0;
+        for (int i = 0; i < n; i++)
+            err += fabs(x1[i] - x[i]);
+
+        err /= n;
+
+        if (err< tol)
+        {
+            cout<<endl<<"The result: "<<endl;
+            for (int i = 0; i < n; i++)
+                cout << "x" << i + 1 << " = " << x1[i] << "\n";
+            cout<<endl<<endl;
+            return;
+        }
+        x = x1;
+    }
+    cout << "Did not converge within the maximum iterations.\n";
+}
+
+void gauss_seidel(vector<int>vec[], int n)
+{
+    vector<double> x(n, 0.0);
+    int maxIt=1000;
+    double tol=0.00001;
+    cout<<endl<<"**********GAUSS-SEIDEL METHOD********"<<endl;
+    for (int it = 0; it < maxIt; it++)
+    {
+        double err = 0.0;
+        for (int i = 0; i < n; i++)
+        {
+            double sum = vec[i][n];
+            for (int j = 0; j < n; j++)
+            {
+                if (i != j)
+                    sum -= vec[i][j] * x[j];
+            }
+            double x1 = sum / vec[i][i];
+            err += fabs(x1 - x[i]);
+            x[i] = x1;
+        }
+        err /= n;
+        if (err < tol)
+        {
+            cout<<endl<<"The result: "<<endl;
+            for (int i = 0; i < n; i++)
+                cout << "x" << i + 1 << " = " << x[i] << "\n";
+            cout<<endl<<endl;
+            return;
+        }
+    }
+    cout << "Did not converge within the maximum iterations.\n";
+}
+
+
 int main()
 {
     int OPcode = 0, method = 0;
@@ -257,10 +330,10 @@ int main()
             }
         }
         if(method == 0){
-            
+          jacobiMethod(ara,n);
         }
         else if(method == 1){
-
+            gauss_seidel(ara,n);
         }
         else if(method == 2){
             gaus(ara, n);
@@ -300,13 +373,13 @@ int main()
 
         }
         else if(method == 6){
-                   false_position(n);
+         false_position(n);
         }
         else if(method == 7){
             
         }
         else if(method == 8){
-                       newton_raphson(n);
+          newton_raphson(n);
         }
      cout << endl<<"Roots found:" << endl;
     for (int i=0; i<ans.size(); i++)
