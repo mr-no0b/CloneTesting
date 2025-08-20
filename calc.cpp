3656883ee611eb291b4dcf02850366bc458b3421
Mujib <x@gmail.com>
2025-08-20 10:55:56 +0600

    Moja

--- calc.cpp
+++ calc.cpp
@@ -2,21 +2,6 @@
 using namespace std;
 
 int main(){
-    int x,y;cin>>x>>y;
-    char c;cin>>c;
-    if(c=='+'){
-        cout<<x+y;
-    }
-    //new line
-    //new line
-    else if(c=='-'){
-        cout<<x-y;
-    }
-    else if(c=='*'){
-        cout<<x*y;
-    }
-    else if(c=='/'){
-        cout<<x/y;
-    }
+    cout<<"testing";
 }
 //changed
