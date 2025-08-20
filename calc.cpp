a0ba1950163cd2c64305a4f1b1fe403c412f9157
Mujib <x@gmail.com>
2025-08-20 11:02:55 +0600

    kisu change

--- calc.cpp
+++ calc.cpp
@@ -1,7 +1,21 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
 int main(){
-    cout<<"testing";
+    int x,y;cin>>x>>y;
+    cout<<"1.Add 2.Substract 3. Multiply 4.Devide"<<endl;
+    int xx;cin>>xx;
+    if(xx==1){
+        cout<<x+y;
+    }
+    else if(xx==2){
+        cout<<x-y;
+    }
+    else if(xx==3){
+        cout<<x*y;
+    }
+    else if(xx==4){
+        cout<<x/y;
+    }
 }
 //changed
