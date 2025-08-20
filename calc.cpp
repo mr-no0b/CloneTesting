be6ae1a383f9b93c48d1fe80e7c73d343164e145
Mujib <x@gmail.com>
2025-08-20 11:00:16 +0600

    vhul hoye gese

--- calc.cpp
+++ calc.cpp
@@ -2,6 +2,20 @@
 using namespace std;
 
 int main(){
-    cout<<"testing";
+    int x,y;cin>>x>>y;
+    cout<<"1.Add 2.Substract 3. Multiply 4.Devide"<<endl;
+    int xx;cin>>xx;
+    if(xx==1){
+        cout<<x+y;
+    }
+    else if(xx==2){
+        cout<<x-y;
+    }
+    else if(xx==3){
+        cout<<x*y;
+    }
+    else if(xx==4){
+        cout<<x/y;
+    }
 }
 //changed
