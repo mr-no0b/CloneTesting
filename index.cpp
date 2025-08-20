844b9305866fa532bd8b56b528321cfe967928fe
Mujib <x@gmail.com>
2025-08-20 10:33:43 +0600

    class added

---
+++ index.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+class testing{
+    int x;
+    testing(int a){
+        x=a;
+    }
+    void put(int a){
+        x=a;
+    }
+    void print(){
+        cout<<x<<endl;
+    }
+};
+int main(){
+    testing t(5);
+    x.print();
+}
